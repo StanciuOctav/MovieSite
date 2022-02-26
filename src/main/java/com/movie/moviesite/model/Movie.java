5998fe39cf1b4767dda35497b942c383fb71adc0
Octav2000 <octavsteaua11@gmail.com>
2022-02-26 16:39:28 +0200

    Connected to Neo4j

--- src/main/java/com/movie/moviesite/model/Movie.java
+++ src/main/java/com/movie/moviesite/model/Movie.java
@@ -1,48 +1,83 @@
 package com.movie.moviesite.model;
 
-import org.neo4j.ogm.annotation.NodeEntity;
-import org.springframework.data.annotation.Id;
 import org.springframework.data.neo4j.core.schema.GeneratedValue;
+import org.springframework.data.neo4j.core.schema.Id;
+import org.springframework.data.neo4j.core.schema.Node;
+import org.springframework.data.neo4j.core.schema.Relationship;
 
-@NodeEntity
+import java.util.Collection;
+
+@Node("MOVIE")
 public class Movie {
-
     @Id
     @GeneratedValue
     private Long id;
-    private String title;
-    private String director;
+    private String name;
+    private String genre;
+    private Integer releaseYear;
 
-    public Movie() {
-    }
+    @Relationship(type = "DIRECTED", direction = Relationship.Direction.INCOMING)
+    private Director director;
 
-    public Movie(Long id, String title, String director) {
+    @Relationship(type = "PLAYED_IN", direction = Relationship.Direction.INCOMING)
+    private Collection<Actor> actors;
+
+    public Movie() {}
+
+    public Movie(Long id, String name, String genre, Integer releaseYear, Director director, Collection<Actor> actors) {
         this.id = id;
-        this.title = title;
+        this.name = name;
+        this.genre = genre;
+        this.releaseYear = releaseYear;
         this.director = director;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public void setDirector(String director) {
-        this.director = director;
+        this.actors = actors;
     }
 
     public Long getId() {
         return id;
     }
 
-    public String getTitle() {
-        return title;
+    public void setId(Long id) {
+        this.id = id;
     }
 
-    public String getDirector() {
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public Integer getReleaseYear() {
+        return releaseYear;
+    }
+
+    public void setReleaseYear(Integer releaseYear) {
+        this.releaseYear = releaseYear;
+    }
+
+    public Director getDirector() {
         return director;
+    }
+
+    public void setDirector(Director director) {
+        this.director = director;
+    }
+
+    public Collection<Actor> getActors() {
+        return actors;
+    }
+
+    public void setActors(Collection<Actor> actors) {
+        this.actors = actors;
     }
 }
