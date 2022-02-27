5475975e782896d8ca982a4b58648c6d72d59fde
Octav2000 <octavsteaua11@gmail.com>
2022-02-27 11:36:12 +0200

    Introduced Lombok

--- src/main/java/com/movie/moviesite/model/Actor.java
+++ src/main/java/com/movie/moviesite/model/Actor.java
@@ -1,5 +1,9 @@
 package com.movie.moviesite.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.neo4j.ogm.annotation.NodeEntity;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.neo4j.core.schema.GeneratedValue;
@@ -10,6 +14,10 @@ import java.util.Collection;
 import java.util.List;
 
 @Node("ACTOR")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Actor {
 
     @Id
@@ -22,63 +30,4 @@ public class Actor {
 
     @Relationship(type = "ACTED_IN")
     private Collection<Movie> movies;
-
-    public Actor() {}
-
-    public Actor(Long id, String name, Integer age, String bornIn, Long netWorth, Collection<Movie> movies) {
-        this.id = id;
-        this.name = name;
-        this.age = age;
-        this.bornIn = bornIn;
-        this.netWorth = netWorth;
-        this.movies = movies;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
-
-    public String getBornIn() {
-        return bornIn;
-    }
-
-    public void setBornIn(String bornIn) {
-        this.bornIn = bornIn;
-    }
-
-    public Long getNetWorth() {
-        return netWorth;
-    }
-
-    public void setNetWorth(Long netWorth) {
-        this.netWorth = netWorth;
-    }
-
-    public Collection<Movie> getMovies() {
-        return movies;
-    }
-
-    public void setMovies(Collection<Movie> movies) {
-        this.movies = movies;
-    }
 }
--- src/main/java/com/movie/moviesite/model/Movie.java
+++ src/main/java/com/movie/moviesite/model/Movie.java
@@ -1,5 +1,9 @@
 package com.movie.moviesite.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.data.neo4j.core.schema.GeneratedValue;
 import org.springframework.data.neo4j.core.schema.Id;
 import org.springframework.data.neo4j.core.schema.Node;
@@ -8,6 +12,10 @@ import org.springframework.data.neo4j.core.schema.Relationship;
 import java.util.Collection;
 
 @Node("MOVIE")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Movie {
     @Id
     @GeneratedValue
@@ -21,68 +29,4 @@ public class Movie {
 
     @Relationship(type = "ACTED_IN", direction = Relationship.Direction.INCOMING)
     private Collection<Actor> actors;
-
-    public Movie() {}
-
-    public Movie(Long id, String name, String genre, Integer releaseYear, Director director, Collection<Actor> actors) {
-        this.id = id;
-        this.name = name;
-        this.genre = genre;
-        this.releaseYear = releaseYear;
-        this.director = director;
-        this.actors = actors;
-    }
-
-    @Override
-    public String toString() {
-        return this.name;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getGenre() {
-        return genre;
-    }
-
-    public void setGenre(String genre) {
-        this.genre = genre;
-    }
-
-    public Integer getReleaseYear() {
-        return releaseYear;
-    }
-
-    public void setReleaseYear(Integer releaseYear) {
-        this.releaseYear = releaseYear;
-    }
-
-    public Director getDirector() {
-        return director;
-    }
-
-    public void setDirector(Director director) {
-        this.director = director;
-    }
-
-    public Collection<Actor> getActors() {
-        return actors;
-    }
-
-    public void setActors(Collection<Actor> actors) {
-        this.actors = actors;
-    }
 }
